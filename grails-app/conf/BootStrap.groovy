6cb16565f7f8c9715ede7486b14e718b890e0f62
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-01-17 15:21:47 -0600

    initial commit

---
+++ grails-app/conf/BootStrap.groovy
@@ -0,0 +1,11 @@
+import edu.umn.idm.User
+import edu.umn.idm.Role
+import edu.umn.idm.UserRole
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
---
+++ test/unit/edu/umn/idm/demo/MenuControllerTests.groovy
@@ -0,0 +1,17 @@
+package edu.umn.idm.demo
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(MenuController)
+class MenuControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
